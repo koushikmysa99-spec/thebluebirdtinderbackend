d833893c791a2057daf1637fff00e9984148c6a3
koushikmysa99-spec <koushikmysa99@gmail.com>
2025-09-14 18:55:08 +0530

    'update'

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ app.use(cors(
     {
         origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+        
+
     }   
 ));
+
+app.set("trust proxy", 1);
 
 
 app.get('/', (req, res) => {
